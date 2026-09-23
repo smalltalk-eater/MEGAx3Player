dd10e4f405f887bb3b8972ac1eb3d7c2c73bafa1
Катя Ельнова <katakata45882@gmail.com>
2026-09-23 10:08:27 +0300

    PLAYER-FINISH-LAB

--- app/src/main/java/com/example/megax3player/val/Theme.kt
+++ app/src/main/java/com/example/megax3player/val/Theme.kt
@@ -1,9 +1,13 @@
 package com.example.megax3player.`val`
 
+import android.os.Build
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 private val LightColors = lightColorScheme(
     primary = KittyPinkLight,
@@ -13,6 +17,25 @@ private val LightColors = lightColorScheme(
     onSecondary = IpodScreenText,
 
     background = IpodBlue,
+    onBackground = IpodScreenText,
+
+    surface = IpodScreen,
+    onSurface = IpodScreenText,
+
+    surfaceVariant = IpodSilver,
+    onSurfaceVariant = IpodScreenText,
+
+    outline = IpodOutline
+)
+
+private val MiddleColors = lightColorScheme(
+    primary = KittyPinkLight,
+    onPrimary = IpodSilverLight,
+
+    secondary = KittyPinkDark,
+    onSecondary = IpodScreenText,
+
+    background = KittyPinkLight,
     onBackground = IpodScreenText,
 
     surface = IpodScreen,
@@ -46,10 +69,29 @@ private val DarkColors = darkColorScheme(
 @Composable
 fun MegaX3PlayerTheme(
     darkTheme: Boolean,
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val context = LocalContext.current
+
+    val colorScheme = when {
+        dynamicColor &&
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+
+            if (darkTheme) {
+                dynamicDarkColorScheme(context)
+            } else {
+                dynamicLightColorScheme(context)
+            }
+        }
+
+        darkTheme -> DarkColors
+
+        else -> MiddleColors
+    }
+
     MaterialTheme(
-        colorScheme = if (darkTheme) DarkColors else LightColors,
+        colorScheme = colorScheme,
         typography = AppTypography,
         content = content
     )
